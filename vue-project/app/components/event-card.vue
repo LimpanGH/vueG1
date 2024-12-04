f7c2a06091b2966aa314fdc0482221d658b2d514
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-12-04 23:53:01 +0100

    upgrade, composables, clean up pages, event + booking component

--- vue-project/app/components/event-card.vue
+++ vue-project/app/components/event-card.vue
@@ -1,4 +1,5 @@
 <template>
+  <<<<<<< HEAD
   <SectionCard>
     <template #header>
       {{ title }}
@@ -13,6 +14,22 @@
       </div>
     </template>
   </SectionCard>
+  =======
+  <SectionCard>
+    <template #header>
+      {{ title }}
+    </template>
+    <img :src="image" alt="" />
+    <template #footer>
+      <div class="space-y-4">
+        <p>{{ description }}</p>
+        <div class="flex justify-end">
+          <RoundButton @click="$emit('register')"> Register </RoundButton>
+        </div>
+      </div>
+    </template>
+  </SectionCard>
+  >>>>>>> 33f2ab8bebfe0354972b618bad451f05a23feeb3
 </template>
 
 <script setup>
--- vue-project/app/layouts/default.vue
+++ vue-project/app/layouts/default.vue
@@ -1,32 +1,48 @@
 <template>
-  <div>
-    <!-- Include the Header component -->
-    <Header :isMenuOpen="isMenuOpen" @toggle-menu="toggleMenu" />
+	<div>
+		<!-- Include the Header component -->
+		<Header :isMenuOpen="isMenuOpen" @toggle-menu="toggleMenu" />
 
-    <main class="container mx-auto p-4">
-      <slot />
-    </main>
-    <footer class="container mx-auto p-4 flex justify-end border-t-2">
-      <ul class="flex gap-4 py-4">
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-      </ul>
-    </footer>
-  </div>
+		<main class="container mx-auto p-4">
+			<slot />
+		</main>
+		<footer class="container mx-auto p-4 flex justify-end border-t-2">
+			<ul class="flex gap-4 py-4">
+				<li><NuxtLink to="/blogs">Blog</NuxtLink></li>
+				<li><NuxtLink to="/booking">Booking</NuxtLink></li>
+				<li><NuxtLink to="/">Home</NuxtLink></li>
+			</ul>
+		</footer>
+	</div>
 </template>
 
 <script setup lang="ts">
 const isMenuOpen = ref(false);
 
 function toggleMenu() {
-  isMenuOpen.value = !isMenuOpen.value;
+	isMenuOpen.value = !isMenuOpen.value;
 }
+useHead({
+	link: [
+		{
+			rel: 'preconnect',
+			href: 'https://fonts.googleapis.com',
+		},
+		{
+			rel: 'stylesheet',
+			href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap',
+			crossorigin: '',
+		},
+	],
+});
 </script>
 
 <style>
+body {
+	font-family: 'Inter', sans-serif;
+}
+
 .router-link-exact-active {
-  color: #12b488;
+	color: #12b488;
 }
 </style>
